97c29feedb823d4839aee1c173eb1c16fa0a68d8
Arsen Kazaryan <arsenk001@mail.ru>
2026-03-16 21:38:04 +0400

    Add resp Headear

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
         <a href="#" className="header__logo">Round Rock <br /><span>Yard Maintenance</span></a>
         <a className="header__link">Lorem Ipsum is simply dummy</a>
         <h1 className="header__title">Lorem ipsum <br />Lorem</h1>
-        <Button name='Help Me'/>
+        <Button name='Help Me' className="Header__btn"/>
       </div>
     </header>
   )
